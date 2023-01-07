b0480bb30d3bb5d91a7311cea9b81063b33d5bdb
Pedro Mouta <pedro.gaspar.mouta@gmail.com>
2023-01-07 19:15:25 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-//for clicking the buttons
 var buttons = document.querySelectorAll(".box");
 for (let i = 0; i < buttons.length; i++) {
 
@@ -8,30 +6,37 @@ for (let i = 0; i < buttons.length; i++) {
         switch (this.innerHTML) {
             case "W":
                 new Audio('sounds/tom-1.mp3').play();
+                animation(".box-1");
                 break;
 
             case "A":
                 new Audio('sounds/tom-2.mp3').play();
+                animation(".box-2");
                 break;
 
             case "S":
                 new Audio('sounds/tom-3.mp3').play();
+                animation(".box-3");
                 break;
 
             case "D":
                 new Audio('sounds/tom-4.mp3').play();
+                animation(".box-4");
                 break;
 
             case "J":
                 new Audio('sounds/snare.mp3').play();
+                animation(".box-5");
                 break;
 
             case "K":
                 new Audio('sounds/crash.mp3').play();
+                animation(".box-6");
                 break;
 
             case "L":
                 new Audio('sounds/kick-bass.mp3').play();
+                animation(".box-7");
                 break;
 
             default: alert("Error, Error!");
@@ -46,132 +51,48 @@ document.addEventListener("keydown", function (e) {
     switch (e.key) {
         case "w":
             new Audio('sounds/tom-1.mp3').play();
-            document.querySelector(".box-1").classList.toggle("active");
-            //document.querySelector(".box-1").style.backgroundColor = "white"
-            //document.querySelector(".box-1").style.opacity = "0.5"
+            animation(".box-1");
             break;
-         
+
         case "a":
             new Audio('sounds/tom-2.mp3').play();
-            document.querySelector(".box-2").classList.toggle("active");
+            animation(".box-2");
             break;
 
         case "s":
             new Audio('sounds/tom-3.mp3').play();
-            document.querySelector(".box-3").classList.toggle("active");
+            animation(".box-3");
             break;
 
         case "d":
             new Audio('sounds/tom-4.mp3').play();
-            document.querySelector(".box-4").classList.toggle("active");
+            animation(".box-4");
             break;
 
         case "j":
             new Audio('sounds/snare.mp3').play();
-            document.querySelector(".box-5").classList.toggle("active");
+            animation(".box-5");
             break;
 
         case "k":
             new Audio('sounds/crash.mp3').play();
-            document.querySelector(".box-6").classList.toggle("active");
+            animation(".box-6");
             break;
 
         case "l":
             new Audio('sounds/kick-bass.mp3').play();
-            document.querySelector(".box-7").classList.toggle("active");
+            animation(".box-7");
             break;
 
         default: console.log("Another key was pressed!");
     }
 });
 
+function animation(box) {
+    document.querySelector(box).classList.add("active");
 
-
-
-
-
-
-
-
-
-
-
-
-// old javascript written....
-
-/*function test() {
-    var a = document.getElementsByClassName("box-1")[0];
-    a.innerHTML = "Audio"
-    a.style.fontSize = "50px"
-    a.style.backgroundColor = "white"
-    a.style.opacity = "0.5";
-}*/
-
-
-// - playing audios immediately, without waiting for them to finish
-/*
-function crash() {
-    var audio = document.getElementById("crash")
-    if (audio.paused) {
-        audio.play();
-    } else {
-        audio.currentTime = 0;
-    }
+    // higher order function setTimeout();
+    setTimeout(function () {
+        document.querySelector(box).classList.remove("active");
+    }, 100)
 }
-
-function kickBass() {
-    var audio = document.getElementById("kickbass")
-    if (audio.paused) {
-        audio.play();
-    } else {
-        audio.currentTime = 0;
-    }
-}
-
-function snare() {
-    var audio = document.getElementById("snare")
-    if (audio.paused) {
-        audio.play();
-    } else {
-        audio.currentTime = 0;
-    }
-}
-
-function tom1() {
-    var audio = document.getElementById("tom1")
-    if (audio.paused) {
-        audio.play();
-    } else {
-        audio.currentTime = 0;
-    }
-}
-
-function tom2() {
-    var audio = document.getElementById("tom2")
-    if (audio.paused) {
-        audio.play();
-    } else {
-        audio.currentTime = 0;
-    }
-}
-
-function tom3() {
-    var audio = document.getElementById("tom3")
-    if (audio.paused) {
-        audio.play();
-    } else {
-        audio.currentTime = 0;
-    }
-}
-
-function tom4() {
-    var audio = document.getElementById("tom4")
-    if (audio.paused) {
-        audio.play();
-    } else {
-        audio.currentTime = 0;
-    }
-}
-
-// - key pressing
-*/
